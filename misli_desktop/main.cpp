fff151ebba6fcf1ff461c67f040e97db9c520a71
Petko Ditchev <pditchev@gmail.com>
2017-04-06 14:16:55 +0300

    Forgot to save main.cpp for the last commit..

--- misli_desktop/main.cpp
+++ misli_desktop/main.cpp
@@ -14,7 +14,7 @@
     along with Misli.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-//release routine
+//release routine...done
 //arch package
 //crash log
 //windows build за маги
@@ -45,7 +45,12 @@
 //За да се ползва FreeType и да излизат по същия начин текстовете трябва да се добави qt.conf в bin (не се е ебавам)
 //директорията, в който да има: [Platforms] \n WindowsArguments = fontengine=freetype
 //bump version: in MisliDesctopGUI and the installer
+//Тук в QtCreator add deployment step: C:\Qt\5.8\mingw53_32\bin\windeployqt.exe --dir data ./release/misli.exe  (в build dir-a)
+//Добавяш misli.exe и qt.conf на ръка в същата папка
+//Select all , десен бутн, 7зип - compress to "data.7z"
+//Cut data.7z and paste to .../installer/packages/com.p10.misli/data
 //build installer PS C:\C++\misli\misli\installer> C:\Qt\QtIFW2.0.0\bin\binarycreator.exe --offline-only -c config\config.xml -p packages MisliInstaller.exe
+//pray to Bill Gates
 
 //=========Test cases===========
 //Input inaccessible/non-existent dir when adding a dir
