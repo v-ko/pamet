bae94a397562a3f8f0c6b69ca25e209c3a158edc
Petko Ditchev <pditchev@gmail.com>
2022-08-21 17:14:42 +0300

    Comment cleanup

--- misli/misli/gui/view_library/view_state.py
+++ misli/misli/gui/view_library/view_state.py
@@ -5,6 +5,7 @@ from typing import Any
 from misli import Entity
 from misli import entity_type
 from misli import gui
+from misli.entity_library.entity import Entity
 
 
 def view_state_type(view_state_class: Any):
@@ -13,14 +14,12 @@ def view_state_type(view_state_class: Any):
 
 @view_state_type
 class ViewState(Entity):
-    _added: bool = field(default=False, repr=False)
-    _version: int = field(default=0, repr=False)
-    # backup: ViewState = field(default=None, repr=False)
+    _added: bool = field(default=False, init=False, repr=False)
+    _version: int = field(default=0, init=False, repr=False)
 
     def __setattr__(self, attr_name, value):
         if self._added and not gui.is_in_action():
             raise Exception('View states can be modified only in actions')
-            # return
 
         Entity.__setattr__(self, attr_name, value)
 
