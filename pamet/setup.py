85eb24eeffd297a2806473cfb7a79701e8bfcfc9
Petko Ditchev <pditchev@gmail.com>
2022-08-28 22:06:08 +0300

    pamet setup.py dependencies update

--- pamet/setup.py
+++ pamet/setup.py
@@ -1,14 +1,15 @@
 import setuptools
 
-setuptools.setup(
-    name='pamet',
-    version='4.0.0.pre-alpha1',
-    packages=setuptools.find_packages(),
-    entry_points={
-        'console_scripts': [
-            'pamet=pamet.desktop_app.main:main',
-            'pamet-debug=pamet.desktop_app.main_debug:main',
-        ],
-    },
-    install_requires=['PySide6', 'misli']
-)
+setuptools.setup(name='pamet',
+                 version='4.0.0.pre-alpha1',
+                 packages=setuptools.find_packages(),
+                 entry_points={
+                     'console_scripts': [
+                         'pamet=pamet.desktop_app.main:main',
+                         'pamet-debug=pamet.desktop_app.main_debug:main',
+                     ],
+                 },
+                 install_requires=[
+                     'PySide6', 'misli', 'click', 'slugify', 'thefuzz',
+                     'peewee', 'pillow'
+                 ])
