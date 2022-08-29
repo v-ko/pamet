416f2dd61664ad9d3c33e7447c817423f1c05008
Petko Ditchev <pditchev@gmail.com>
2022-08-29 19:02:24 +0300

    Separate the repo from the original misli repo. Move pamet files to root folder

--- misli/fusion/basic_classes/__init__.py
+++
@@ -1,3 +0,0 @@
-from .point2d import Point2D
-from .rectangle import Rectangle
-from .color import Color
--- misli3-legacy/misli_desktop/archivemodule.cpp
+++
@@ -1,9 +0,0 @@
-#include "archivemodule.h"
-
-ArchiveModule::ArchiveModule(Timeline *timeline_):
-    TimelineModule(timeline_),
-    checkbox("Archive module")
-{
-    controlWidget = &checkbox;
-    noteFile.setPathAndLoad("/sync/misli/.misli_timeline_database.json");
-}
