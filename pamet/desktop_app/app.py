e252ff1a36c09169a319b6c95e3506c29e74f4b3
Petko Ditchev <pditchev@gmail.com>
2022-10-10 20:09:28 +0300

    Fixed exception presenting

--- pamet/desktop_app/app.py
+++ pamet/desktop_app/app.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QApplication
+import traceback
+from PySide6.QtWidgets import QApplication, QMessageBox
 
 import fusion
 from fusion.logging import LoggingLevels, LOGGING_LEVEL
@@ -20,3 +21,8 @@ class DesktopApp(QApplication):
         self.setApplicationName(name)
         self.setApplicationVersion(pamet.__version__)
         self.setQuitOnLastWindowClosed(True)  # Needed for some reason
+
+    def present_exception(self, exception: Exception, title: str = None):
+        title = title or 'Exception raised'
+        text = str(exception) + '\n\nTraceback:\n' + traceback.format_exc()
+        QMessageBox.critical(self.activeWindow(), title, text)
