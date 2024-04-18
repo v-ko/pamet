502bf1df0e6a33d468b62cc8dbff28570d335583
Petko Ditchev <pditchev@gmail.com>
2024-04-18 17:57:29 +0300

    Updated generated qt ui classes to 6.3

--- server/pamet/views/tab/ui_widget.py
+++ server/pamet/views/tab/ui_widget.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'widget.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.3.1
+## Created by: Qt User Interface Compiler version 6.5.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -78,20 +78,6 @@ class Ui_TabMainWidget(object):
 
         self.centralLayout.addWidget(self.mapPageContainer)
 
-        self.tourContainer = QWidget(self.centralContainer)
-        self.tourContainer.setObjectName(u"tourContainer")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-        sizePolicy3.setHorizontalStretch(0)
-        sizePolicy3.setVerticalStretch(0)
-        sizePolicy3.setHeightForWidth(self.tourContainer.sizePolicy().hasHeightForWidth())
-        self.tourContainer.setSizePolicy(sizePolicy3)
-        self.horizontalLayout_3 = QHBoxLayout(self.tourContainer)
-        self.horizontalLayout_3.setSpacing(0)
-        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
-
-        self.centralLayout.addWidget(self.tourContainer)
-
 
         self.verticalLayout_3.addLayout(self.centralLayout)
 
@@ -100,11 +86,11 @@ class Ui_TabMainWidget(object):
 
         self.rightSidebarContainer = QWidget(TabMainWidget)
         self.rightSidebarContainer.setObjectName(u"rightSidebarContainer")
-        sizePolicy4 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
-        sizePolicy4.setHorizontalStretch(5)
-        sizePolicy4.setVerticalStretch(0)
-        sizePolicy4.setHeightForWidth(self.rightSidebarContainer.sizePolicy().hasHeightForWidth())
-        self.rightSidebarContainer.setSizePolicy(sizePolicy4)
+        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        sizePolicy3.setHorizontalStretch(5)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.rightSidebarContainer.sizePolicy().hasHeightForWidth())
+        self.rightSidebarContainer.setSizePolicy(sizePolicy3)
         self.rightSidebarContainer.setMinimumSize(QSize(100, 0))
         self.verticalLayout_2 = QVBoxLayout(self.rightSidebarContainer)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
